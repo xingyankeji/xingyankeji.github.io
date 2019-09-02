4652f517919d464fea2e7cdbe90d2c80c924dc7c
靳艳锋 <yanfeng10@staff.weibo.com>
2019-09-02 15:36:40 +0800

    Site updated: 2019-09-02 15:36:40

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@
         return;
       }
     } else {
-      var html = '<div class="bdsharebuttonbox bdshare-button-style0-16" data-bd-bind="1524625953372" style="position: absolute;right: 50px;"> <a href="#" class="bds_more" data-cmd="more"></a> <a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a> <a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a> <a href="#" class="bds_tqq" data-cmd="tqq" title="分享到腾讯微博"></a> <a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a> </div>';
+      var html = '';
 
       var box = $(html);
 
